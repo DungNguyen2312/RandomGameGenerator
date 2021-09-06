b4ae240a3b5c95a0901728601380de5c728ee740
Tri Dung Nguyen / BBM3H19M <Tri_Dung.Nguyen@edu.bib.de>
2021-09-06 08:32:10 +0200

    Projekt anlegt

---
+++ RandomGameGenerator/RandomGameGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomGameGenerator
+//
+//  Created by Tri Dung Nguyen / BBM3H19M on 06.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
